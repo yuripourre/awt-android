8f9ead7e9639024ace360130b8f1150bb39b5bc6
Yuri Pourre <yuriamayuri@gmail.com>
2016-10-15 08:30:26 -0400

    Add color class

---
+++ src/main/java/java/awt/Color.java
@@ -0,0 +1,41 @@
+package java.awt;
+
+public class Color {
+	
+	public static final int BLACK = 0xff000000;
+	public static final int black = BLACK;
+	
+	public static final int BLUE = 0xff0000ff;
+	public static final int blue = BLUE;
+	
+	public static final int CYAN = 0xff00ffff;
+	public static final int cyan = CYAN;
+	
+	public static final int DARK_GRAY = 0xff404040;
+	public static final int darkGray = DARK_GRAY;
+	
+	public static final int GREEN = 0xff00ff00;
+	public static final int green = GREEN;
+	
+	public static final int LIGHT_GRAY = 0xffc0c0c0;
+	public static final int lightGray = LIGHT_GRAY;
+	
+	public static final int MAGENTA = 0xffff00ff;
+	public static final int magenta = MAGENTA;
+	
+	public static final int ORANGE = 0xffffc800;
+	public static final int orange = ORANGE;
+	
+	public static final int PINK = 0xffaf00af;
+	public static final int pink = PINK;
+	
+	public static final int RED = 0xffff0000;
+	public static final int red = RED;
+	
+	public static final int YELLOW = 0xffffff00;
+	public static final int yellow = YELLOW;
+	
+	public static final int WHITE = 0xffffffff;
+	public static final int white = WHITE;
+
+}
